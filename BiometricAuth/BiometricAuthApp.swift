6f46311b53764ff011b34996a93b14e5c0a6c090
Joshua Butler <butler.josh101@gmail.com>
2024-03-18 18:39:58 -0400

    Initial Commit

---
+++ BiometricAuth/BiometricAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricAuthApp.swift
+//  BiometricAuth
+//
+//  Created by Joshua Butler on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct BiometricAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BiometricAuth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BiometricAuth
+//
+//  Created by Joshua Butler on 3/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
